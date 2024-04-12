cfeecd0c642774bb424753b996c23bb1bfa8b4e5
Sabrina Sauter <sas@format-d.com>
2024-04-12 14:04:22 +0200

    chore: use new cr models and interfaces in form processors.

--- Classes/NodeType/SelectOptionsCreationHandler.php
+++ Classes/NodeType/SelectOptionsCreationHandler.php
@@ -1,26 +1,32 @@
 <?php
 namespace Neos\Form\Builder\NodeType;
 
-use Neos\ContentRepository\Domain\Model\NodeInterface;
+
 use Neos\Neos\Ui\NodeCreationHandler\NodeCreationHandlerInterface;
+use Neos\ContentRepository\Core\ContentRepository;
+use Neos\ContentRepository\Core\Feature\NodeCreation\Command\CreateNodeAggregateWithNode;
 
 class SelectOptionsCreationHandler implements NodeCreationHandlerInterface
 {
+
     /**
-     * @param NodeInterface $node The newly created node
-     * @param array $data incoming data from the creationDialog
-     * @return void
+     * @param CreateNodeAggregateWithNode $command The original node creation command
+     * @param array<string|int,mixed> $data incoming data from the creationDialog
+     * @return CreateNodeAggregateWithNode the original command or a new creation command with altered properties
      */
-    public function handle(NodeInterface $node, array $data)
+    public function handle(CreateNodeAggregateWithNode $command, array $data, ContentRepository $contentRepository): CreateNodeAggregateWithNode
     {
-        if (!$node->getNodeType()->isOfType('Neos.Form.Builder:SelectOption')) {
-            return;
+        if (!$contentRepository->getNodeTypeManager()->getNodeType($command->nodeTypeName)->isOfType('Neos.Form.Builder:SelectOption')) {
+            return $command;
         }
+
         if (isset($data['value'])) {
-            $node->setProperty('value', $data['value']);
+            $propertyValues = $propertyValues->withValue('value', $data['value']);
         }
         if (isset($data['label'])) {
-            $node->setProperty('label', $data['label']);
+            $propertyValues = $propertyValues->withValue('label', $data['label']);
         }
+
+        return $command->withInitialPropertyValues($propertyValues);
     }
 }
